c2b7c3feb800b19b64ff5788258ab1e6fec37b61
user <user@Users-MacBook-Pro-2.local>
2023-08-01 11:05:17 -0600

    adding custom middleware in perfil route

--- backend/routes/veterinarianRoutes.js
+++ backend/routes/veterinarianRoutes.js
@@ -5,19 +5,23 @@ import {
   readToken,
   auth,
 } from "../controllers/veterinarianController.js";
+import authMiddleware from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
 //El callback (req,=> lo que se manda, res => la respuesta) se manda llamar automaticamente al llamar la ruta
-
+//PUBLIC ACCOUNTS
 router.post("/", register);
-
 //Adding a dynamic parameter
 //We need to send the token in order to be readble
 router.get("/readToken/:token", readToken);
 
-router.get("/perfil", perfil);
-
 router.post("/login", auth);
+
+//PRIVATE ACCOUNTS with Custom middleware
+
+//Once perfil is visited, then open the function (executes their function),
+// and then next goes to perfil
+router.get("/perfil", authMiddleware, perfil);
 
 export default router;
