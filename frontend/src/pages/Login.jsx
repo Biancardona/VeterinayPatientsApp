5b1116f05e3357c1b0cbc68cef0a7a3d07a16fbf
user <user@Users-MacBook-Pro-2.local>
2023-08-11 11:06:33 -0600

    Creating login form

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,7 +1,41 @@
 const Login = () => {
   return (
     <>
-      <h2>Login</h2>
+      <div>
+        <h1 className="text-indigo-600 font-black text-6xl">
+          Inicia Sesion y Administra tus
+          <spam className="text-black"> Pacientes</spam>
+        </h1>
+      </div>
+      <div>
+        <form>
+          <div className="my-5">
+            <label className="uppercase text-gray-600 block text-xl font-bold">
+              Email
+            </label>
+            <input
+              type="email"
+              placeholder="Email de Registro"
+              className="border w-full p-3 mt-3 bg-gray-50 rounded-xl"
+            />
+          </div>
+          <div className="my-5">
+            <label className="uppercase text-gray-600 block text-xl font-bold">
+              Password
+            </label>
+            <input
+              type="password"
+              placeholder="Password"
+              className="border w-full p-3 mt-3 bg-gray-50 rounded-xl"
+            />
+            <input
+              type="submit"
+              value="Iniciar Sesion"
+              className="border w-full p-3 mt-5 my-5 px-10 bg-indigo-600 text-white uppercase rounded-xl font-bold hover:cursor-pointer hover:bg-indigo-900"
+            />
+          </div>
+        </form>
+      </div>
     </>
   );
 };
