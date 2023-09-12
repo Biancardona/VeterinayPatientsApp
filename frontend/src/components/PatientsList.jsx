e2bcf037dfed4f82a6c293192ade19c20e4f8646
user <user@Users-MacBook-Pro-2.local>
2023-09-12 12:00:29 -0600

    Conditional way to show patients list

--- frontend/src/components/PatientsList.jsx
+++ frontend/src/components/PatientsList.jsx
@@ -1,5 +1,36 @@
+import usePatient from "../hooks/usePatient";
+
 const PatientsList = () => {
-  return <div>PatientsList</div>;
+  const { patients } = usePatient();
+  console.log(patients);
+
+  return (
+    <>
+      {patients.length ? (
+        <>
+          {" "}
+          <h2 className="text-3xl font-bold text-center">
+            Listado de pacientes
+          </h2>{" "}
+          <p className="text-xl text-center mt-5 mb-10">
+            Administra tus{" "}
+            <spam className="text-indigo-600">Pacientes y Citas </spam>{" "}
+          </p>
+        </>
+      ) : (
+        <>
+          {" "}
+          <h2 className="text-3xl font-bold text-center">
+            No hay pacientes
+          </h2>{" "}
+          <p className="text-xl text-center mt-5 mb-10">
+            Comienza agregando tus{" "}
+            <spam className="text-indigo-600">pacientes </spam>{" "}
+          </p>
+        </>
+      )}{" "}
+    </>
+  );
 };
 
 export default PatientsList;
